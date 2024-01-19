4fe0fa1fdf8c3fa260b4a4c671e07c00d34193c1
Noor Muhammad <noorrasheed317@gmail.com>
2024-01-19 10:40:47 +0500

    Configure the Graph Ql on front-end

--- src/index.js
+++ src/index.js
@@ -11,26 +11,28 @@ import { Provider as ReduxProvider } from 'react-redux';
 // apex-chart
 import 'assets/third-party/apex-chart.css';
 
+// Apollo Client
+import { ApolloProvider } from '@apollo/client/react';
+import client from 'apollo';
+
 // project import
 import App from './App';
 import { store } from 'store';
 import reportWebVitals from './reportWebVitals';
 
-// ==============================|| MAIN - REACT DOM RENDER  ||============================== //
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(
   <StrictMode>
-    <ReduxProvider store={store}>
-      <BrowserRouter basename="/">
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    <ApolloProvider client={client} >
+      <ReduxProvider store={store}>
+        <BrowserRouter basename="/">
+          <App />
+        </BrowserRouter>
+      </ReduxProvider>
+    </ApolloProvider>
   </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
