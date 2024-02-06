ae59066bf047fd69182349ab1799cf9d400f5cb6
Noor Muhammad <noorrasheed317@gmail.com>
2024-02-06 15:25:20 +0500

    update the  URL

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-	uri: "http://localhost:8080/graphql",
+	uri: "https://test-test.adaptable.app/graphql",
 	cache: new InMemoryCache(),
 });
 
 export default client;
+
+//http://localhost:8080/graphql
